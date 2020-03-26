4d0022aabafbe7e3ae6424730005c61e8a155a85
nombregenerico <62416784+nombregenerico@users.noreply.github.com>
2020-03-26 15:24:24 -0500

    version 2

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ Created on Thu Mar 26 12:01:58 2020
 
 a= int(input("ingrese el valor de a: "))
 b= int(input("ingrese el valor de b: "))
+d= int(input("ingrese el valor de d: "))
 
 #aqui se realizan las operaciones y se imprimen
 
-print("el doble de a es:"+str(int(a*2)))
+print("el doble de a es:"+ str(int(a*2)))
 print("el residuo de la division es: "+ str(int(a%b)))
+print("el cuadrado de b es: "+ str(int(b**2)))
+print("la raiz cuadrada de d es: "+str(int(d**(1/2))))
 
 
