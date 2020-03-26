06601dc5550c8d4835868c17923e2c4350731a6c
nombregenerico <62416784+nombregenerico@users.noreply.github.com>
2020-03-26 12:37:21 -0500

    mi primer commit

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ Created on Thu Mar 26 12:01:58 2020
 @author: abaly
 """
 
+#&& ejercicio 5
+
+#aqui se ponen las entradas
+
+a= int(input("ingrese el valor de a: "))
+b= int(input("ingrese el valor de b: "))
+
+#aqui se realizan las operaciones y se imprimen
+
+print("el doble de a es:"+str(int(a*2)))
+print("el residuo de la division es: "+ str(int(a%b)))
+
 
