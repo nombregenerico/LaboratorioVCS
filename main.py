1d18a21f9da77e432f8ac9d9de22b4a81873ebc1
nombregenerico <62416784+nombregenerico@users.noreply.github.com>
2020-03-26 16:47:35 -0500

    cuadratica agregada, error

--- main.py
+++ main.py
@@ -20,4 +20,20 @@ print("el residuo de la division es: "+ str(int(a%b)))
 print("el cuadrado de b es: "+ str(int(b**2)))
 print("la raiz cuadrada de d es: "+str(int(d**(1/2))))
 
-
+#&& ejercicio 12
+#aqui se ingresan las variables
+a= int(input("ingrese a: "))
+b= int(input("ingrese b: "))
+c= int(input("ingrese c: "))
+#aqui se aplica la formula 
+d= (b**2)-(4*a*c)
+#aqui usamos condicional para hallar las x
+if d>0 :
+    print("x1: "+str(((-b)+(d**(1/2))/(2*a))))
+    print("x2: "+str(((-b)+(d**(1/2))/(2*a))))
+elif d==0 :
+    print("x1 y x2 son iguales, y su valor es: "+str((-b)/(2*a)))
+elif d<0:
+    print("i")
+    
+    
